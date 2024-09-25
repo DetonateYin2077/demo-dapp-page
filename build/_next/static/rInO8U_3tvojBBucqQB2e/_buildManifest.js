295a342ac7f5fd92b130e3df4cc7f3d4b6860e79
Devin Yin <117371221+DetonateYin2077@users.noreply.github.com>
2024-09-25 18:39:23 +0800

    Add files via upload

---
+++ build/_next/static/rInO8U_3tvojBBucqQB2e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-75cc6d467997efa6.js"],"/_error":["static/chunks/pages/_error-b5ebfc4bb90c11db.js"],"/origin":["static/chunks/pages/origin-e8022f256f50c753.js"],sortedPages:["/","/_app","/_error","/origin"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
